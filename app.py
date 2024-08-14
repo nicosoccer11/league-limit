9bb52b84c2d3d97d3634e45609770aefa95a89c0
Nico <nicolas17g@gmail.com>
2024-08-14 12:29:15 -0500

    Next step is to add prevention of opening league client

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import requests
+import os
+import subprocess
+import time
 from datetime import datetime, timedelta
 from config import API_KEY
 
@@ -18,17 +21,18 @@ def get_most_recent_match_by_puuid(puuid: str) -> str:
     response = requests.get(url)
     return response.json()[0]
 
-def get_match_results(match_id: str, puuid: str) -> bool:
+def get_match_results_and_date(match_id: str, puuid: str) -> bool:
     ''' 
     Returns the match results (win(T)/loss(F)) of the given matchId for the given PUUID
     '''
     url = f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={API_KEY}'
     response = requests.get(url)
     particpants = response.json()['info']['participants']
+    gameEndUnix = response.json()['info']['gameEndTimestamp']
     for i in range(len(particpants)):
         if particpants[i]['puuid'] == puuid:
-            return particpants[i]['win']
-
+            return particpants[i]['win'], gameEndUnix
+        
 def get_seconds_until_6am() -> int:
     '''
     Returns the number of seconds remaining until 6am
@@ -40,3 +44,42 @@ def get_seconds_until_6am() -> int:
     seconds_until_6am = (target_time - now).total_seconds()
 
     return int(seconds_until_6am)
+
+def get_date() -> datetime.date:
+    '''
+    Gets the current date
+    '''
+    now = datetime.now()
+
+    return str(now.date())
+
+def unix_to_date(unix_time: int) -> str:
+    ''' 
+    Converts a Unix timestamp in milliseconds to a human-readable date.
+    '''
+    # Convert milliseconds to seconds
+    unix_time_seconds = unix_time / 1000
+    
+    return datetime.fromtimestamp(unix_time_seconds).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def close_league_client():
+    '''
+    Closes the League of Legends client
+    '''
+    # This is Windows
+    if os.name == 'nt':
+        print("Running on Windows")
+        subprocess.call(["taskkill", "/F", "/IM", "LeagueClient.exe"])
+        subprocess.call(["taskkill", "/F", "/IM", "LeagueClientUx.exe"])
+    # This is a Unix-like system (Linux, macOS)
+    elif os.name == 'posix':
+        print("Running on a Unix-like system")
+        subprocess.call(["pkill", "LeagueClient"])
+        subprocess.call(["pkill", "LeagueClientUx"])
+    else:
+        print("Unsupported operating system")
+
+def prevent_league_access(seconds_until_6am):
+    # Sleep until 6 AM
+    time.sleep(seconds_until_6am)
--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-import os
-import subprocess
-import time
 import app
 from config import SUMMONER_NAME, TAGLINE
-
-def close_league_client():
-    if os.name == 'nt':
-        # This is Windows
-        print("Running on Windows")
-        subprocess.call(["taskkill", "/F", "/IM", "LeagueClient.exe"])
-        subprocess.call(["taskkill", "/F", "/IM", "LeagueClientUx.exe"])
-    elif os.name == 'posix':
-        # This is a Unix-like system (Linux, macOS)
-        print("Running on a Unix-like system")
-        subprocess.call(["pkill", "LeagueClient"])
-        subprocess.call(["pkill", "LeagueClientUx"])
-    else:
-        print("Unsupported operating system")
-
-def prevent_league_access(seconds_until_6am):
-    # Sleep until 6 AM
-    time.sleep(seconds_until_6am)
 
 def main():
 
@@ -31,11 +10,14 @@ def main():
     match_id = app.get_most_recent_match_by_puuid(puuid)
 
     # Get match results (win(T)/loss(F))
-    match_result = app.get_match_results(match_id, puuid)
+    match_result, gameEndUnix = app.get_match_results_and_date(match_id, puuid)
+    
+    # Get the date of the match
+    unix_to_date = app.unix_to_date(gameEndUnix)
 
-    # if match_result is False, close the league client
-    if match_result == False:
-        close_league_client
+    # if match_result is False, and date of match is == to current day close the league client
+    if match_result == False and unix_to_date[0:10] == app.get_date():
+        app.close_league_client()
     else:
         pass
 
